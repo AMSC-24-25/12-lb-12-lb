39b489a52168cd50249e663806b804e931245a9e
FedericaRavasio01 <fede.ravasio01@gmail.com>
2024-12-12 17:07:17 +0100

    Update LBM_2.cpp
    BC conditions and correction to equilibrium

--- code/LBM_2.cpp
+++ code/LBM_2.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <fstream>
+#include <cstring> 
 #include <vector>
 #include <array>
 #include <cmath>
@@ -25,10 +26,10 @@ class LBmethod{
     const double nu = (u_lid * L) / Re;  // Kinematic viscosity calculated using Re
     const double tau = 3.0 * nu + 0.5;    // Relaxation time for BGK collision model
 
-
+    
 
     // Define D2Q9 lattice directions (velocity directions for D2Q9 model)
-    const std::vector<std::pair<int, int>> direction = {
+    const std::array<std::pair<int, int>, 9> direction = {
         std::make_pair(0, 0),   // Rest direction
         std::make_pair(1, 0),   // Right
         std::make_pair(0, 1),   // Up
@@ -40,7 +41,7 @@ class LBmethod{
         std::make_pair(1, -1)   // Bottom-right diagonal
     };
     // D2Q9 lattice weights
-    const std::vector<double> weight = {
+    const std::array<double, 9> weight = {
         4.0 / 9.0,  // Weight for the rest direction
         1.0 / 9.0,  // Right
         1.0 / 9.0,  // Up
@@ -67,14 +68,12 @@ class LBmethod{
    
     //Methods:
     void Initialize(){
-        
-    //Vectors to store simulation data:
+        //Vectors to store simulation data:
         rho.assign(NX * NY, rho0); // Density initialized to rho0 everywhere
         u.assign(NX * NY, {0.0, 0.0}); // Velocity initialized to 0
         f_eq.assign(NX * NY * ndirections, 0.0); // Equilibrium distribution function array
         f.assign(NX * NY * ndirections, 0.0); //  Distribution function array
 
-        PrintDensity();
 
         // Apply boundary condition: set velocity at the top lid (moving lid)
         for (unsigned int x = 0; x < NX; ++x) {
@@ -83,7 +82,6 @@ class LBmethod{
             u[INDEX(x, y, NX)].second = 0.0;  // Vertical velocity is 0 at the top lid
         }
 
-        PrintVelocity();
         
         // Compute the equilibrium distribution function f_eq
         for (unsigned int x = 0; x < NX; ++x) {
@@ -100,11 +98,130 @@ class LBmethod{
 
                     // Compute f_eq using the BGK collision formula
                     f_eq[INDEX3D(x, y, i, NX, ndirections)] = weight[i] * rho[idx] * (1.0 + 3.0 * cu + 4.5 * cu * cu - 1.5 * u2);
+                    std::memcpy(f.data(), f_eq.data(), f.size() * sizeof(double));
                 }
             }
         }
+        std::cout << "Equilibrium (initial state):\n";
+        PrintDensity();
+        PrintVelocity();
         PrintDistributionF();
-        Save_equilibrium(rho, u, f_eq);
+        //unsigned int t=0;
+        //Save_Output(t);
+        
+        
+    }
+
+    void UpdateMacro(){
+        for (unsigned int x=0; x<NX; ++x){
+            for (unsigned int y = 0; y < NY; ++y) {
+                size_t idx = INDEX(x, y, NX);
+                double rho_local = 0.0;
+                double ux_local = 0.0;
+                double uy_local = 0.0;
+                for (unsigned int i = 0; i < ndirections; ++i) {
+                    const double fi=f[INDEX3D(x, y, i, NX, ndirections)];
+                    rho_local += fi;
+                    ux_local += fi * direction[i].first;
+                    uy_local += fi * direction[i].second;
+                }
+                if (rho_local > 1e-10) {
+                    ux_local /= rho_local;
+                    uy_local /= rho_local;
+                }
+                rho[idx]=rho_local;
+                u[idx].first=ux_local;
+                u[idx].second=uy_local;
+                for (unsigned int i = 0; i < ndirections; ++i){
+                    double ux = u[idx].first; // Horizontal velocity at point (x, y)
+                    double uy = u[idx].second; // Vertical velocity at point (x, y)
+                    double u2 = ux * ux + uy * uy; // Square of the speed magnitude
+                    double cx = direction[i].first; // x-component of direction vector
+                    double cy = direction[i].second; // y-component of direction vector
+                    double cu = (cx * ux + cy * uy); // Dot product (c_i · u)
+                    f_eq[INDEX3D(x, y, i, NX, ndirections)] = weight[i] * rho[idx] * (1.0 + 3.0 * cu + 4.5 * cu * cu - 1.5 * u2);
+                } 
+            }
+        }
+    }
+
+    void Collisions(){
+        //we use f=f-(f-f_eq)/tau from BGK
+        for (unsigned int x=0;x<NX;++x){
+            for (unsigned int y=0;y<NY;++y){
+                for (unsigned int i=0;i<ndirections;++i){
+                    f[INDEX3D(x, y, i, NX, ndirections)]=f[INDEX3D(x, y, i, NX, ndirections)]-(f[INDEX3D(x, y, i, NX, ndirections)]-f_eq[INDEX3D(x, y, i, NX, ndirections)])/tau;
+                }
+            }
+        }
+    }
+
+    void Streaming(){
+        //f(x,y,t+1)=f(x-cx,y-cy,t)
+        std::vector<double> f_temp(NX * NY * ndirections, 0.0); // distribution function array temporaneal
+        for (unsigned int x=0;x<NX;++x){
+            for (unsigned int y=0;y<NY;++y){
+                for (unsigned int i=0;i<ndirections;++i){
+                    int x_str = x - direction[i].first;
+                    int y_str = y - direction[i].second;
+                    //check for particles inside the walls (sorta BC but they will be applyied more specifically after)
+                    if(x_str<0) x_str=1;//bounceback (only position not velocity)
+                    if(x_str>=NX) x_str=NX-1;//bounceback
+                    if(y_str<0) y_str=1;//bounceback
+                    if(y_str>=NY) y_str=NY-1;//bounceback
+                    //apply straming function
+                    f_temp[INDEX3D(x, y, i, NX, ndirections)] = f[INDEX3D(x_str, y_str, i, NX, ndirections)];
+                }
+            }
+        }
+        std::swap(f, f_temp);//f_temp is f at t=t+1 so now we use the new function f_temp in f
+    }
+
+    void BC() {
+        //(since NX=NY we use only one outer for)
+        for(unsigned int a=0; a<NX; ++a) {
+            for(unsigned int i = 0; i < ndirections; ++i){
+                //TOP BOUNDARY: (LID-VELOCITY)
+                if (direction[i].second < 0) { 
+                    //fi=f_opposite - 6*wi*rho*ci*u_lid --> from lattice Boltzamann equilibrium function and momentum conservation
+                    size_t opp = INDEX3D(a, NY-1, (i + 4) % ndirections, NX, ndirections); // find the opposite direction of i
+                    f[INDEX3D(a, NY-1, i, NX, ndirections)] = f[opp] - 6.0 * weight[i] * rho[INDEX(a,NY-1,NX)] * direction[i].first * u_lid;    
+                }
+                //BOTTOM BOUNDARY: (NO-SLIP)(Stationary Wall)
+                if (direction[i].second> 0) { 
+                    size_t opp = INDEX3D(a, 0, (i + 4) % ndirections, NX, ndirections); // find the opposite direction of i
+                    f[INDEX3D(a, 0, i, NX, ndirections)] = f[opp];
+                }
+                //LEFT BOUNDARY:
+                if (direction[i].first> 0) { 
+                    size_t opp = INDEX3D(0, a, (i + 4) % ndirections, NX, ndirections);
+                    f[INDEX3D(0, a, i, NX, ndirections)] = f[opp];
+                }
+                //RIGHT BOUNDARY:
+                if (direction[i].first < 0) {
+                    size_t opp = INDEX3D(NX-1, a, (i + 4) % ndirections, NX, ndirections);
+                    f[INDEX3D(NX - 1, a, i, NX, ndirections)] = f[opp];
+                }
+            }
+        }
+
+    }
+    
+    void Run_simulation(){
+        for (unsigned int t=0; t<NSTEPS; ++t){
+            Collisions();
+            Streaming();
+            BC();
+            UpdateMacro();
+            if (t%2==0){
+                Save_Output(t);
+            }
+            std::cout << "\n";
+            std::cout << "Step: "+std::to_string(t+1)<< std::endl;
+            PrintDensity();
+            PrintVelocity();
+            PrintDistributionF();
+        }
     }
 
 
@@ -129,119 +246,51 @@ class LBmethod{
     }
 
     void PrintDistributionF(){
-        // Print the computed f_eq values for debugging purposes
-        std::cout << "Equilibrium distribution f_eq:\n";
+        // Print the computed f values for debugging purposes
+        std::cout << "Distribution function:\n";
         for (unsigned int x = 0; x < NX; ++x) {
             for (unsigned int y = 0; y < NY; ++y) {
                 size_t idx = INDEX(x, y, NX);
                 std::cout << "Point (" << x << ", " << y << "): ";
                 for (unsigned int i = 0; i < ndirections; ++i) {
-                    std::cout << f_eq[INDEX3D(x, y, i, NX, ndirections)] << " ";
+                    std::cout << f[INDEX3D(x, y, i, NX, ndirections)] << " ";
                 }
                 std::cout << "\n";
             }
         }
     }
 
-    void Save_equilibrium(const std::vector<double>& rho, const std::vector<std::pair<double, double>>& u, const std::vector<double>& f_eq) {
-        std::ofstream file("output_equilibrium.csv");
+    void Save_Output(unsigned int t) {
+        std::ofstream file("output_" + std::to_string(t) + ".csv");
         if (!file.is_open()) {
             std::cerr << "Errore: impossibile aprire il file." << std::endl;
             return;
         }
 
-        file << "x,y,rho,u_x,u_y";
+        file << "x,y,u_x,u_y,rho";
         for (unsigned int i = 0; i < ndirections; ++i) {
-            file << ",f_eq_" << i;
+            file << ",f" << i;
         }
         file << "\n";
 
         for (unsigned int x = 0; x < NX; ++x) {
             for (unsigned int y = 0; y < NY; ++y) {
                 size_t idx = INDEX(x, y, NX);
-                file << x << "," << y << "," << rho[idx] << "," << u[idx].first << "," << u[idx].second;
+                file << x << "," << y << "," << u[idx].first << "," << u[idx].second<< "," << rho[idx] ;
                 for (unsigned int i = 0; i < ndirections; ++i) {
-                    file << "," << f_eq[INDEX3D(x, y, i, NX, ndirections)];
+                    file << "," << f[INDEX3D(x, y, i, NX, ndirections)];
                 }
                 file << "\n";
             }
         }
-
         file.close();
-        std::cout << "File salvato" << std::endl;
+        std::cout << "File at t = " + std::to_string(t) + " saved" << std::endl;
     }
-
-    void UpdateMacro(){
-        for (unsigned int x = 0; x < NX; ++x) {
-            for (unsigned int y = 0; y < NY; ++y) {
-                size_t idx = INDEX(x, y, NX);
-                double rho_local = 0.0;
-                double ux_local = 0.0;
-                double uy_local = 0.0;
-                for (unsigned int i = 0; i < ndirections; ++i) {
-                    const double fi=f[INDEX3D(x, y, i, NX, ndirections)];
-                    rho_local += fi;
-                    ux_local += fi * direction[i].first;
-                    uy_local += fi * direction[i].second;
-                }
-
-                if (rho_local > 1e-10) {
-                    ux_local /= rho_local;
-                    uy_local /= rho_local;
-                }
-                rho[idx]=rho_local;
-                u[idx].first=ux_local;
-                u[idx].second=uy_local;
-            }
-        }
-    }
-    void Collisions(){
-        //we use f=f-(f-f_eq)/tau from BGK
-        for (unsigned int x=0;x<NX;++x){
-            for (unsigned int y=0;y<NY;++y){
-                for (unsigned int i=0;i<ndirections;++i){
-                    f[INDEX3D(x, y, i, NX, ndirections)]=f[INDEX3D(x, y, i, NX, ndirections)]-(f[INDEX3D(x, y, i, NX, ndirections)]-f_eq[INDEX3D(x, y, i, NX, ndirections)])/tau;
-                }
-            }
-        }
-    }
-    void Streaming(){
-        //f(x,y,t+1)=f(x-cx,y-cy,t)
-        std::vector<double> f_temp(NX * NY * ndirections, 0.0); // distribution function array temporaneal
-        for (unsigned int x=0;x<NX;++x){
-            for (unsigned int y=0;y<NY;++y){
-                for (unsigned int i=0;i<ndirections;++i){
-                    int x_str = x - direction[i].first;
-                    int y_str = y - direction[i].second;
-                    //check for particles inside the walls (sorta BC but they will be applyied more specifically after)
-                    if(x_str<0) x_str=1;//bounceback (only position not velocity)
-                    if(x_str>=NX) x_str=NX-1;//bounceback
-                    if(y_str<0) y_str=1;//bounceback
-                    if(y_str>=NY) y_str=NY-1;//bounceback
-
-                    //apply straming function
-                    f_temp[INDEX3D(x, y, i, NX, ndirections)] = f[INDEX3D(x_str, y_str, i, NX, ndirections)];
-                }
-            }
-        }
-        std::swap(f, f_temp);//f_temp is f at t=t+1 so now we use the new function f_temp in f
-    }
-    void BC(){}
-    void Run_simulation(const unsigned int NSTEPS){
-        for (unsigned int t=0;t<NSTEPS;++t){
-            Streaming();
-            Collisions();
-            BC();
-            UpdateMacro();
-            Save_output();
-        }
-    }
-    void Save_output(){}
     
 };
 
 int main(){
-    const unsigned int NSTEPS = 10;       // Number of timesteps to simulate
+    const unsigned int NSTEPS = 15;       // Number of timesteps to simulate
     const unsigned int NX = 5;           // Number of nodes in the x-direction
     const double u_lid = 1.0;            // Lid velocity at the top boundary
     const double Re = 100.0;             // Reynolds number
@@ -250,7 +299,7 @@ int main(){
 
     LBmethod lb(NSTEPS,NX,u_lid,Re,L,rho);
     lb.Initialize();
-    lb.Run_simulation(NSTEPS);
+    lb.Run_simulation();
 
     std::cout << "Simulation completed." << std::endl;
     return 0;
