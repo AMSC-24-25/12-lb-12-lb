5c28d315cc94c4469c8879dba5113b331cd9a524
Lorenzo-Bonelli <57605813+Lorenzo-Bonelli@users.noreply.github.com>
2024-12-16 23:20:15 +0100

    Update compile_and_run.sh
    added number of cores required from outside

--- code/compile_and_run.sh
+++ code/compile_and_run.sh
@@ -1,15 +1,20 @@
 #!/bin/bash
 
-# Line to run: copy the two lines below in the prompt
-# chmod +x compile_and_run.sh
-# ./compile_and_run.sh
+#LINES to print #substitue 8 with the desired number of cores
+#chmod +x compile_and_run.sh
+#./compile_and_run.sh 8
 
+# Usage instructions
+if [ "$#" -ne 1 ]; then
+  echo "Usage: ./compile_and_run.sh <number_of_cores>"
+  exit 1
+fi
 # Compile the program
 gcc-10 -std=c++17 -fopenmp $(pkg-config --cflags --libs opencv4) -o LBM_esec LBM_2.cpp -lopencv_core -lopencv_highgui -lopencv_imgproc -lopencv_imgcodecs -lstdc++ -lm
 
 
 # Run the program
-./LBM_esec
+./LBM_esec $NUM_CORES
 
 # Generate video from frames
 if [ -d "frames" ]; then
