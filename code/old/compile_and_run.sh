5a8144493c3cfda1fd8cd9b4d20798add3c8ab87
Lorenzo-Bonelli <57605813+Lorenzo-Bonelli@users.noreply.github.com>
2024-12-19 12:47:42 +0100

    Update compile_and_run.sh

--- code/old/compile_and_run.sh
+++ code/old/compile_and_run.sh
@@ -3,20 +3,20 @@
 # Usage:
 # ./compile_and_run.sh <version> [number_of_threads]
 # Where:
-#   <version> is 1, 2, or 3
+#   <version> is 1, 2, 3, or 4
 #   [number_of_threads] is optional and only allowed for version 3
 
 # Check for valid input
 if [ "$#" -lt 1 ] || [ "$#" -gt 2 ]; then
   echo "Usage: ./compile_and_run.sh <version> [number_of_threads]"
-  echo "       <version> can be 1, 2, or 3"
+  echo "       <version> can be 1, 2, 3, or 4"
   echo "       [number_of_threads] is only allowed for version 3"
   exit 1
 fi
 
-# Check if the first argument is valid (1, 2, or 3)
-if [[ "$1" != "1" && "$1" != "2" && "$1" != "3" ]]; then
-  echo "Error: Invalid version '$1'. Valid options are 1, 2, or 3."
+# Check if the first argument is valid (1, 2, 3, or 4)
+if [[ "$1" != "1" && "$1" != "2" && "$1" != "3" && "$1" != "4" ]]; then
+  echo "Error: Invalid version '$1'. Valid options are 1, 2, 3, or 4."
   exit 1
 fi
 
@@ -32,7 +32,7 @@ if [ "$LBM_VERSION" == "3" ]; then
   fi
   echo "Running LBM_$LBM_VERSION with $NUM_CORES threads"
 elif [ "$#" -eq 2 ]; then
-  # For LBM_1 or LBM_2, thread count is not allowed
+  # For LBM_1, LBM_2, or LBM_4, thread count is not allowed
   echo "Error: Thread count is not allowed for LBM_$LBM_VERSION."
   exit 1
 else
@@ -40,6 +40,19 @@ else
 fi
 
 # Compile the appropriate file
+if [ "$LBM_VERSION" == "4" ]; then
+  echo "Compiling and running with g++-10..."
+  g++-10 -O3 -Wall -Wextra -march=native -std=c++17 -fopenmp $(pkg-config --cflags --libs opencv4) -o simulation_exec main.cpp LBM.cpp \
+    -lopencv_core -lopencv_highgui -lopencv_imgproc -lopencv_imgcodecs -lstdc++ -lm
+  if [ $? -ne 0 ]; then
+    echo "Compilation failed."
+    exit 1
+  fi
+  echo "Running simulation_exec..."
+  ./simulation_exec
+  exit 0
+fi
+
 echo "Compiling LBM_$LBM_VERSION..."
 gcc-10 -std=c++17 -fopenmp $(pkg-config --cflags --libs opencv4) -o LBM_esec LBM_$LBM_VERSION.cpp -lopencv_core -lopencv_highgui -lopencv_imgproc -lopencv_imgcodecs -lstdc++ -lm
 if [ $? -ne 0 ]; then
