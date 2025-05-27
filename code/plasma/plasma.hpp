e6c4c8ee7e702683260d11465b54734cf116e848
FedericaRavasio01 <fede.ravasio01@gmail.com>
2025-05-27 11:06:18 +0200

    Update plasma.hpp

--- code/plasma/plasma.hpp
+++ code/plasma/plasma.hpp
@@ -19,16 +19,24 @@ private:
     const size_t NSTEPS;
     const size_t NX;
     const size_t NY;
-    const double Re;
     const size_t num_cores;
 
     // Fixed parameters
     const size_t ndirections = 9;
+    const size_t Z_ion;
+    const size_t A_ion;
+    const double r_ion;
     const double tau_ion;
     const double tau_el;
-    const double q_ion=1.6021766 * 1e-19;
-    const double q_el= - 1.6021766 * 1e-19;;
-    const double sigma = 10.0;
+    const double q_el= - 1.6021766 * 1e-19; //C
+    const double kb=1.380649*1e-23; //J/K
+    const double m_el=-9.010938356*1e-31; //Kg
+    const double m_p=1.672621922369*1e-27;//Kg
+    const double eps_0=8.8541878128*1e-12;//F/m
+    const double r_el=2.8179*1e-15;//m
+    const double Bz_ext = 0.0;  // magnitude of magnetic field in z-direction
+    const double Ex_ext = 100.0;  // magnitude of electric field in x-direction
+    const double Ey_ext = 0.0;  // magnitude of electric field in x-direction
 
     // Directions and weights for D2Q9
     const std::array<int, 9> directionx;
@@ -67,11 +75,19 @@ private:
         return i + ndirections * (x + NX * y);
     }
 
+    //Function to reduce the lenght of program
+    inline void apply_colormap(const cv::Mat& input_32f, cv::Mat& output_8uc3, int colormap) {
+        static cv::Mat tmp_8u;
+        cv::normalize(input_32f, tmp_8u, 0, 255, cv::NORM_MINMAX);
+        tmp_8u.convertTo(tmp_8u, CV_8U);
+        cv::applyColorMap(tmp_8u, output_8uc3, colormap);
+    }
+
     cv::VideoWriter video_writer;
 
 public:
     // Constructor
-    LBmethod(const size_t NSTEPS, const size_t NX, const size_t NY, const double Re, const size_t num_cores, const size_t tau_ion ,const size_t tau_el);
+    LBmethod(const size_t NSTEPS, const size_t NX, const size_t NY, const size_t num_cores, const size_t Z_ion, const size_t A_ion, const double r_ion, const double tau_ion, const double tau_el);
 
     // Methods
     void Initialize();
