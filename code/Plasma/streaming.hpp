7f03ef5c89185f70f924308e868a2ee1e9d2028e
Lorenzo-Bonelli <57605813+Lorenzo-Bonelli@users.noreply.github.com>
2025-06-19 23:08:29 +0200

    Update streaming.hpp

--- code/Plasma/streaming.hpp
+++ code/Plasma/streaming.hpp
@@ -1,5 +1,4 @@
-#ifndef STREAMING_HPP
-#define STREAMING_HPP
+#pragma once
 
 #include "utils.hpp"
 
@@ -12,34 +11,54 @@ enum class BCType {
     Periodic,
     BounceBack
 };
-/// Dispatcher generico per scegliere il tipo di streaming su f e g
-void Stream(std::vector<double>& fe, std::vector<double>& fi, std::vector<double>& fn, 
+//──────────────────────────────────────────────────────────────────────────────
+//  Streaming dispatcher:
+//──────────────────────────────────────────────────────────────────────────────
+void Stream(std::vector<double>& f_e, std::vector<double>& f_i, std::vector<double>& f_n, 
             std::vector<double>& temp_e, std::vector<double>& temp_i, std::vector<double>& temp_n, 
-            std::vector<double>& ge, std::vector<double>& gi, std::vector<double>& gn,
-            const std::array<int,Q>& cx, const std::array<int,Q>& cy, const std::array<int, Q>& opp,
+            std::vector<double>& g_e, std::vector<double>& g_i, std::vector<double>& g_n,
+            const std::array<int,Q>& cx, const std::array<int,Q>& cy,
             const int NX, const int NY, const BCType type);
 
-/// Streaming massico (f) 
-void StreamingPeriodic(std::vector<double>& fe, std::vector<double>& fi, std::vector<double>& fn,
+//──────────────────────────────────────────────────────────────────────────────
+//  Streaming + PERIODIC boundaries:
+//    f_{i}(x + c_i, y + c_i, t+1) = f_{i}(x,y,t)
+//──────────────────────────────────────────────────────────────────────────────
+void StreamingPeriodic(std::vector<double>& f_e, std::vector<double>& f_i, std::vector<double>& f_n,
                        std::vector<double>& temp_e, std::vector<double>& temp_i, std::vector<double>& temp_n, 
                        const std::array<int,Q>& cx, const std::array<int,Q>& cy,
                        const int NX, const int NY);
-void StreamingBounceBack(std::vector<double>& fe, std::vector<double>& fi, std::vector<double>& fn,
+//──────────────────────────────────────────────────────────────────────────────
+//  Streaming + BOUNCE‐BACK walls at x=0, x=NX−1, y=0, y=NY−1.
+//    If (x + c_i,x) or (y + c_i,y) is out‐of‐bounds, reflect:
+//       f_{i*}(x,y) += f_{i}(x,y),
+//    where i* = opp[i].
+//──────────────────────────────────────────────────────────────────────────────
+void StreamingBounceBack(std::vector<double>& f_e, std::vector<double>& f_i, std::vector<double>& f_n,
                        std::vector<double>& temp_e, std::vector<double>& temp_i, std::vector<double>& temp_n, 
-                       const std::array<int, Q>& cx, const std::array<int, Q>& cy, const std::array<int, Q>& opp,
+                       const std::array<int, Q>& cx, const std::array<int, Q>& cy,
                        const int NX, const int NY);
 
-/// Streaming termico (g)
-void ThermalStreamingPeriodic(std::vector<double>& ge, std::vector<double>& gi, std::vector<double>& gn,
+//──────────────────────────────────────────────────────────────────────────────
+//  Streaming + PERIODIC boundaries:
+//    g_{i}(x + c_i, y + c_i, t+1) = g_{i}(x,y,t)l
+//──────────────────────────────────────────────────────────────────────────────
+void ThermalStreamingPeriodic(std::vector<double>& g_e, std::vector<double>& g_i, std::vector<double>& g_n,
                               std::vector<double>& temp_e, std::vector<double>& temp_i, std::vector<double>& temp_n, 
                               const std::array<int,Q>& cx, const std::array<int,Q>& cy,
                               const int NX, const int NY);
-void ThermalStreamingBounceBack(std::vector<double>& ge, std::vector<double>& gi, std::vector<double>& gn,
+//──────────────────────────────────────────────────────────────────────────────
+//  Streaming + BOUNCE‐BACK walls at x=0, x=NX−1, y=0, y=NY−1.
+//    If (x + c_i,x) or (y + c_i,y) is out‐of‐bounds, reflect:
+//       g_{i*}(x,y) += g_{i}(x,y),
+//    where i* = opp[i].
+//  That condition correspond to a Neumann BC so to a zero normal flux at wall
+//──────────────────────────────────────────────────────────────────────────────
+void ThermalStreamingBounceBack(std::vector<double>& g_e, std::vector<double>& g_i, std::vector<double>& g_n,
                        std::vector<double>& temp_e, std::vector<double>& temp_i, std::vector<double>& temp_n, 
-                       const std::array<int, Q>& cx, const std::array<int, Q>& cy, const std::array<int, Q>& opp,
+                       const std::array<int, Q>& cx, const std::array<int, Q>& cy,
                        const int NX, const int NY);
 
 
 }  // namespace streaming
 
-#endif  // STREAMING_HPP
